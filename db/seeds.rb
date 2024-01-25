f751cabdf271203cabc2def60d19e31a35c916d4
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-24 17:46:39 -0700

    Seed data to test API with localhost and Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+book_1 = Book.create(
+  title: "The Green Bay Tree",
+  author: "Leonore Marvin",
+  genre: "Crime/Detective",
+  summary: "Dicta velit modi. Recusandae soluta iusto. Provident sed excepturi.",
+  number_sold: 584,
+  popularity: "high"
+)
+book_2 = Book.create(
+  title: "Number the Stars",
+  author: "Marc Klocko",
+  genre: "Fiction narrative",
+  summary: "Molestiae commodi itaque. Tenetur harum rerum. Rerum nobis et.",
+  number_sold: 52,
+  popularity: "high"
+)
+book_3 = Book.create(
+  id: 45,
+  title: "Number the Stars",
+  author: "Mimi Powlowski",
+  genre: "Mystery",
+  summary: "Magnam ut sunt. Ducimus aspernatur consequatur. Consequatur sit voluptatem.",
+  number_sold: 2,
+  popularity: "low"
+)
